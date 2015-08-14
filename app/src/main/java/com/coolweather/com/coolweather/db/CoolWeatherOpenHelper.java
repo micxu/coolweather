0a13bca80742f79a3e91edccf1d1949ac8040102
Mixu <Miaochunxu@outlook.com>
2015-08-14 08:20:19 +0800

    create

--- app/src/main/java/com/coolweather/com/coolweather/db/CoolWeatherOpenHelper.java
+++ app/src/main/java/com/coolweather/com/coolweather/db/CoolWeatherOpenHelper.java
@@ -45,9 +45,9 @@ public class CoolWeatherOpenHelper extends SQLiteOpenHelper
     @Override
     public void onCreate(SQLiteDatabase db)
     {
-        db.execSQL(CREATE_PROVINCE);
-        db.execSQL(CREATE_CITY);
-        db.execSQL(CREATE_COUNTY);
+        db.execSQL(CREATE_PROVINCE); //创建Province表
+        db.execSQL(CREATE_CITY);     //创建City表
+        db.execSQL(CREATE_COUNTY);   //创建County表
     }
 
     @Override
